fd382771969f21400afb9f2cb7894ff4381a88aa
root <root@research.ju.edu.et>
2021-11-21 10:30:14 +0300

    detatils

--- src/Controller/SubmissionController.php
+++ src/Controller/SubmissionController.php
@@ -272,6 +272,22 @@ return $this->redirectToRoute('submission_index');
     public function metadata(Request $request, CallForProposal $callForProposal, UserController $test, MailerInterface $mailer): Response {
         $this->denyAccessUnlessGranted('ROLE_USER');
 
+
+#######################
+        $em  = $this->getDoctrine()->getManager();
+       $lastdate = $em->getRepository('App:CallForProposal')->find($callForProposal);
+
+	$deadline = $lastdate->getDeadline();
+	#echo $deadline;
+        $today = new \DateTime('');
+        $message = '';
+        if ($deadline <= $today) {
+       $flashbag = $this->get('session')->getFlashBag();
+            $flashbag->add("danger", "Sorry! Call has expired!  Thank you!");
+           return $this->redirectToRoute('myreviews');
+        }
+
+################################
         ##########################
         $userdetails = $this->getUser()->getUserInfo();
         if ($userdetails == '') {
