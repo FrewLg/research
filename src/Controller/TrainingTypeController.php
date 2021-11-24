ce7109454af686ba6eeee10b8985fa621ecc382d
firew legese sarmiso <firew.legese@ju.edu.et>
2021-11-24 15:06:03 +0300

    certificate

--- src/Controller/TrainingTypeController.php
+++ src/Controller/TrainingTypeController.php
@@ -11,7 +11,7 @@ use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
-#[Route('/training/type')]
+#[Route('/training-type')]
 class TrainingTypeController extends AbstractController
 {
     #[Route('/', name: 'training_type_index', methods: ['GET'])]
