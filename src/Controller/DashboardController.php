b21fbde037aeec89b87bd30581c8285a35730c18
firew legese sarmiso <firew.legese@ju.edu.et>
2021-12-08 12:15:38 +0300

    excel for submission title

--- src/Controller/DashboardController.php
+++ src/Controller/DashboardController.php
@@ -275,23 +275,27 @@ class DashboardController extends AbstractController {
          $submissions = $em->getRepository(Submission::class)->findAll(); 
         $spreadsheet = new Spreadsheet(); 
         /* @var $sheet \PhpOffice\PhpSpreadsheet\Writer\Xlsx\Worksheet */
+
+        
         $sheet = $spreadsheet->getActiveSheet();
         $sheet->setCellValue('A1', 'No.');
-        $sheet->setCellValue('B1', 'PI');
-        $sheet->setCellValue('C1', 'Co-PI (s)');
-        $sheet->setCellValue('D1', 'PI\'s Institute');
-        $sheet->setCellValue('E1', 'PI\'s College');
+        $sheet->setCellValue('B1', 'Title.');
+        $sheet->setCellValue('C1', 'PI');
+        $sheet->setCellValue('D1', 'Co-PI (s)');
+        $sheet->setCellValue('E1', 'PI\'s Institute');
+        $sheet->setCellValue('F1', 'PI\'s College');
         $sheet->setTitle("Researcher"); 
         $counter = 2;
         foreach ($submissions as $phoneNumber) {
             $sheet->setCellValue('A' . $counter, $phoneNumber->getId());
+            $sheet->setCellValue('B' . $counter, $phoneNumber->getTitle());
             $counter2 = 2; 
             ########################
-            $sheet->setCellValue('B' . $counter, $phoneNumber->getAuthor()->getUserInfo());
-            $sheet->setCellValue('D' . $counter, $phoneNumber->getAuthor()->getUserInfo()->getCollege());
-            $sheet->setCellValue('E' . $counter, $phoneNumber->getAuthor()->getUserInfo()->getDepartment());
+            $sheet->setCellValue('C' . $counter, $phoneNumber->getAuthor()->getUserInfo());
+            $sheet->setCellValue('E' . $counter, $phoneNumber->getAuthor()->getUserInfo()->getCollege());
+            $sheet->setCellValue('F' . $counter, $phoneNumber->getAuthor()->getUserInfo()->getDepartment());
              foreach ($phoneNumber->getCoAuthors() as $CoAuthors) {
-             $sheet->setCellValue('C' . $counter, $CoAuthors->getResearcher()->getUserInfo());
+             $sheet->setCellValue('D' . $counter, $CoAuthors->getResearcher()->getUserInfo());
              $counter++;
             $counter2++; 
        
