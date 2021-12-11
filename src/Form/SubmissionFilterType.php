b7b2a8e697906ec6706ed73640c76a531372d43a
hmhard <miniye6453@gmail.com>
2021-12-11 11:50:14 +0300

    changes made

--- src/Form/SubmissionFilterType.php
+++ src/Form/SubmissionFilterType.php
@@ -17,34 +17,32 @@ class SubmissionFilterType extends AbstractType
     {
         $builder
             ->add('abstract')
-           
-            
+
+
             ->add('title')
-            ->add('sent_at',DateTimeType::class,[
-                "required"=>false
+            ->add('sent_at', DateTimeType::class, [
+                "required" => false
             ])
             ->add('uidentifier')
             ->add('complete')
             ->add('submission_type')
-           ->add('funding_organization')
+            ->add('funding_organization')
             ->add('project_start_at')
             ->add('project_end_at')
             ->add('progress')
-           ->add('published',ChoiceType::class,[
-"choices"=>[
-    "Published"=>1,
-    "not Published"=>0,
-]
-           ])
+            ->add('published', ChoiceType::class, [
+                "choices" => [
+                    "Published" => 1,
+                    "not Published" => 0,
+                ]
+            ])
             ->add('status')
             ->add('keywords')
             ->add('author')
-            ->add('thematic_area',EntityType::class,[
-                "class"=>ThematicArea::class,
-                "required"=>false
-            ])
-       
-        ;
+            ->add('thematic_area', EntityType::class, [
+                "class" => ThematicArea::class,
+                "required" => false
+            ]);
     }
 
     public function configureOptions(OptionsResolver $resolver): void
