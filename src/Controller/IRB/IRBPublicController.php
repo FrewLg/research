e7b6e132b5988a3ac1314714731fbdc1d9b309ed
Abubeker Seifu <abubeker3522@gmail.com>
2022-02-16 09:15:21 +0300

    123

--- src/Controller/IRB/IRBPublicController.php
+++ src/Controller/IRB/IRBPublicController.php
@@ -7,6 +7,7 @@ use App\Helper\DomPrint;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\HttpKernel\Exception\AccessDeniedHttpException;
 use Symfony\Component\Routing\Annotation\Route;
 
 class IRBPublicController extends AbstractController
@@ -17,7 +18,7 @@ class IRBPublicController extends AbstractController
         $em=$this->getDoctrine()->getManager();
         if($request->request->get('validate')){
            $irbCertificate= $em->getRepository(IrbCertificate::class)->findOneBy(['certificateCode'=>$request->request->get('validate')]);
-            if(!$irbCertificate){ $this->addFlash('error','Invalid clearance code');}
+            if(!$irbCertificate){ $this->addFlash('error','No IRB clearance was issued with "'.$request->request->get('validate').'" code');}
             else{
                 return $this->render('irb/clearance.html.twig', [
                     'irb'=>$irbCertificate
@@ -25,6 +26,9 @@ class IRBPublicController extends AbstractController
             }
         }
         if($request->query->get('export')){
+            if($irbCertificate->getIrbApplication()->getSubmittedBy() != $this->getUser()){
+               return new AccessDeniedHttpException(); 
+            }
            return  new Response($domPrint->print("irb/print.html.twig",["certificate"=>$irbCertificate],"PRINT",DomPrint::ORIENTATION_PORTRAIT,DomPrint::PAPER_A4,true));
         }
 
