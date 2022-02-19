ca297df2ad787bf950982fc74af25142ddc5b600
firew legese sarmiso <firew.legese@ju.edu.et>
2022-02-19 12:48:21 +0300

    percentages

--- src/Controller/IRB/IRBReviewAssignmentController.php
+++ src/Controller/IRB/IRBReviewAssignmentController.php
@@ -63,7 +63,7 @@ class IRBReviewAssignmentController extends AbstractController
 
         if ($submission->getSubmittedBy() == $this->getUser()) {
             $this->addFlash('danger', 'Sorry! You can not assign by yourself a reviewer to the submission you made!');
-            return $this->redirectToRoute('submission_index');
+            return $this->redirectToRoute('application_index');
         }
         ///// check if the submission is completed or not 
         $reviewAssignment = new IRBReviewAssignment();
@@ -87,15 +87,13 @@ class IRBReviewAssignmentController extends AbstractController
 
                 return $this->redirectToRoute('irb_review_assignment_new', array('id' => $submission->getId()));
 
-
-
+ 
             $reviewAssignment->setApplication($submission);
             $duedate = $reviewAssignment->getDuedate();
             $reviewAssignment->setInvitationSentAt(new \DateTime());
             // dd($submission->getId());
             $reviewAssignment->getApplication()->setStatus($entityManager->getRepository(IRBStatus::class)->find(2));
-            
-            
+             
             $entityManager->persist($reviewAssignment);
             $entityManager->flush();
             $this->addFlash(
@@ -104,7 +102,7 @@ class IRBReviewAssignmentController extends AbstractController
             );
             $suffix = $reviewAssignment->getIRBReviewer();
             $theFirstName = $reviewAssignment->getIRBReviewer()->getUserInfo()->getFirstName();
-            $invitation_url = "irb-review/" . $reviewAssignment->getId() . "/accept/";
+            $invitation_url = "irb/reviewer-assignment/" . $reviewAssignment->getId() . "/revise/";
             $theEmail = $reviewAssignment->getIRBReviewer()->getEmail();
 
 
--- src/Controller/ReviewAssignmentController.php
+++ src/Controller/ReviewAssignmentController.php
@@ -183,7 +183,7 @@ class ReviewAssignmentController extends AbstractController
             $body = $messages->getBody();
             $title = $submission->getTitle();
             $theFirstName = $reviewAssignment->getReviewer()->getUserInfo()->getFirstName();
-            $invitation_url = "irb-review/" . $reviewAssignment->getId() . "/accept/";
+            $invitation_url = "subm-review/" . $reviewAssignment->getId() . "/accept/";
             $theEmail = $reviewAssignment->getReviewer()->getEmail();
             $email = (new TemplatedEmail())
                 ->from(new Address('research@ju.edu.et', $this->getParameter('app_name')))
@@ -296,7 +296,7 @@ class ReviewAssignmentController extends AbstractController
             $subject = $messages->getSubject();
             $body = $messages->getBody();
             $title = $submission->getTitle();
-            $invitation_url = "irb-review/" . $reviewAssignment->getId() . "/accept/";
+            $invitation_url = "subm-review/" . $reviewAssignment->getId() . "/accept/";
             $theEmail = $reviewAssignment->getReviewer()->getEmail();
             $email = (new TemplatedEmail())
                 ->from(new Address('research@ju.edu.et', $this->getParameter('app_name')))
