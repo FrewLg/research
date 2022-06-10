471fa9fd3f7d19af9feb3498814fe3739206c159
firew legese sarmiso <firew.legese@ju.edu.et>
2022-06-10 18:12:21 +0300

    Changes has been made 10-06-2022 18:12:21

--- gitlabpull.sh
+++ gitlabpull.sh
@@ -4,7 +4,8 @@
 git add .
 git commit -m "Changes has been made $(date +'%d-%m-%Y %H:%M:%S')"
 git pull
-#./bin/console doctrine:schema:update --force 
+
+git push #./bin/console doctrine:schema:update --force 
 #rm -r var/cache/*
 #rm -r var/*
 #mkdir var
